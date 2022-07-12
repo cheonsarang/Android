2af64363012a095410339ebb48cbe0741051feed
sarang <sarang012834@gmail.com>
2022-07-12 17:43:17 +0900

    2022/07/12 And

---
+++ And01/app/src/main/java/com/example/and01/StudentMain.java
@@ -0,0 +1,14 @@
+package com.example.and01;
+
+public class StudentMain {
+    //주석
+    //변수선언
+    int num1, num2;
+
+    public static void main(String[] args) {
+        //num1에 10담기
+        StudentMain studentMain = new StudentMain();
+        studentMain.num1 = 10;
+        System.out.println(studentMain.num1);
+    }
+}
